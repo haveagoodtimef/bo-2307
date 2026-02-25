8da87fb9616b0fc35a2c498558918018cd4cf34c
feng <78236491@qq.com>
2026-02-25 16:25:39 +0800

    加测试的注释

--- app/src/main/java/com/fenghongzhang/youbo_2307/MainActivity.kt
+++ app/src/main/java/com/fenghongzhang/youbo_2307/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+/**
+ * 这是首页
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
